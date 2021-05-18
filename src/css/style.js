ada8151729368e64521b897514da2d4f7c739f1b
khadetou <khadetou96@gmail.com>
2021-05-18 18:28:55 +0000

    starter

---
+++ src/css/style.js
@@ -0,0 +1,13 @@
+import { makeStyles } from '@material-ui/core/styles';
+
+
+//Varibles
+const lightGreen = "#7ed56f";
+const mediumGreen = "#55c57a"
+const darkGreen = "#28b485";
+
+
+export const useStyle = makeStyles({
+
+});
+
